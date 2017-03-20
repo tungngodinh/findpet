3b526579f322f5a18de42b9fdcaf700a73ca542d
tungnd_citigo <tung.nd@citigo.net>
2017-03-20 19:21:27 +0700

    feat(NewCell):Set image

--- FindPet/Views/FPNewCell.m
+++ FindPet/Views/FPNewCell.m
@@ -6,13 +6,34 @@
 //  Copyright © 2017 tungnd. All rights reserved.
 //
 
+#import <FontAwesomeKit/FontAwesomeKit.h>
+
 #import "FPNewCell.h"
+#import "FPConstant.h"
 
 @implementation FPNewCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
+    
+    CGSize iconsize = CGSizeMake(10, 10);
+    
+    FAKIonIcons *icon = [FAKIonIcons iosHeartOutlineIconWithSize:10];
+    [icon setAttributes:@{NSForegroundColorAttributeName:FPBackgroundColor}];
+    self.likeImageView.image = [icon imageWithSize:iconsize];
+    
+    icon = [FAKIonIcons iosComposeOutlineIconWithSize:10];
+    [icon setAttributes:@{NSForegroundColorAttributeName:FPBackgroundColor}];
+    self.commentImageView.image = [icon imageWithSize:iconsize];
+    
+    icon = [FAKIonIcons androidShareIconWithSize:10];
+    [icon setAttributes:@{NSForegroundColorAttributeName:FPBackgroundColor}];
+    self.shareImageView.image = [icon imageWithSize:iconsize];
+    
+    self.avataImageView.layer.cornerRadius = 25;
+    self.avataImageView.layer.borderWidth = 1;
+    self.avataImageView.layer.borderColor = FPBackgroundColor.CGColor;
+    self.avataImageView.layer.masksToBounds = YES;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
